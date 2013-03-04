4d8ae47f45ed32cabea11aa6ed7d8423d0277812
Socapex <philippe_groarke@yahoo.ca>
2013-03-04 16:32:14 -0500

    First small test

---
+++ Orchestra/src/mainPhil.cpp
@@ -0,0 +1,30 @@
+/********************************************************************************//**
+ * @file	main.cpp
+ * @author	Alexandre Arsenault <alexandre.arsenault@polymtl.ca>
+ * @brief	Main Application
+ * @date	Creation 06/02/2013
+ ***********************************************************************************/
+#include "mainPhil.h"
+
+IMPLEMENT_APP(Orchestra);
+
+#define SEL_ONE  SELECT_ONLY_THIS_ID
+
+bool Orchestra::OnInit()
+{
+	frame = new MainFrame(axT("Orchestra"));
+	
+	frame->Show();
+	SetTopWindow(frame);
+
+	return true;
+}
+
+MainFrame::MainFrame(const axString& title): axFrame(NULL, axID_ANY, title, axPoint(30, 30), axSize(500, 500))
+{
+	SetBackgroundColour(axColor(80, 80, 80));
+	mainPanel = new axPanel(this, axID_ANY, axPoint(0, 0), this->GetSize());
+	mainPanel->SetBackgroundColour(axColor(80, 80, 80));
+
+    ControlBar* test = new ControlBar(mainPanel, wxID_ANY, wxPoint(0 , this->GetSize().y - 100), wxSize(this->GetSize().x, 100));
+}
